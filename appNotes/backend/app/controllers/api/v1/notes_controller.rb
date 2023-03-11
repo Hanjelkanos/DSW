e51c29620cf56a85666cb0ece6ed0c2038eb8404
Roberto Ortuño Blanco <91477159+robertoOB07@users.noreply.github.com>
2023-03-11 20:49:25 +0100

    Add files via upload

---
+++ appNotes/backend/app/controllers/api/v1/notes_controller.rb
@@ -0,0 +1,54 @@
+class Api::V1::NotesController < ApplicationController
+  def index
+    notes = Note.all()
+    render json:notes, status: 200
+  end
+
+  def new
+    note = Note.new
+  end
+
+  def create
+    note = Note.new(title: note_params[:title], body: note_params[:body], author: note_params[:author], date:note_params[:date], image:note_params[:image])
+    if note.save
+      render json:note, status: 200
+    else
+      render json: {error: "creating error..."}
+    end
+  end
+
+  def show
+    note = Note.find_by(id:params[:id])
+    if note
+      render json:note, status: 200
+    else
+      render json: {error: "Note not found!"}
+    end
+  end
+
+  def update
+    note = Note.find(params[:id])
+
+    if note
+      note.update(title: params[:title], body: params[:body], author: params[:author], date:params[:date], image:params[:image])
+      render json: "Note updated successfully"
+    else
+      render json:{ error: "Note not found"}
+    end
+  end
+
+  
+
+  def destroy
+    note = Note.find(params[:id])
+    if note
+      note.destroy
+      render json: "Note deleted successfully"
+    end
+  end
+
+  private
+    def note_params
+      params.require(:note).permit(:title, :body, :author, :date, :image)
+    end
+end
---
+++ appNotes/backend/db/migrate/20230226153603_create_notes.rb
@@ -0,0 +1,13 @@
+class CreateNotes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.text :body
+      t.string :author
+      t.string :date
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
